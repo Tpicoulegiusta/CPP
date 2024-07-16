634db1f6a6885066472fe0fffe165a4f29fe0b24
Tpicoulegiusta <tpicoule123@gmail.com>
2024-07-16 16:28:28 +0200

    scalar almost finished

--- Cpp_06/ex00/ScalarConverter.hpp
+++ Cpp_06/ex00/ScalarConverter.hpp
@@ -17,6 +17,12 @@
 #include <climits>
 #include <cstring>
 #include <iomanip>
+#include <cstdlib>
+#include <limits.h>
+#include <limits>
+#include <cctype>
+#include <sstream>
+
 
 class ScalarConverter
 {
@@ -32,6 +38,15 @@ public:
     static int is_float(std::string str);
     static int is_double(std::string str);
     static char convert_to_char(std::string str);
+    static int convert_to_int(std::string str);
+    static bool check(std::string str);
+    static float convert_to_float(std::string str);
+    static bool check_float(std::string str);
+    static bool check_int_1(std::string str);
+    static double convert_to_double(std::string str);
+
+
+
 
 };
 
