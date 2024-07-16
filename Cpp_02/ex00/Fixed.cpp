87b09158576a040a9920a48bdd3b85b784e3dbe6
Tpicoulegiusta <tpicoule123@gmail.com>
2024-07-16 16:28:27 +0200

    added ex00 fixed

---
+++ Cpp_02/ex00/Fixed.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 13:43:50 by tpicoule          #+#    #+#             */
+/*   Updated: 2024/05/14 16:50:40 by tpicoule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+    this->value = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+	
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+    this->value = rhs.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->value = raw;
+}
+
+Fixed::Fixed(const Fixed &cpy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = cpy; 
+}
