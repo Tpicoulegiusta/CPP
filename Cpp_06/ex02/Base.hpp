9631428fd9b40d5473992c27e0118df31a569101
Tpicoulegiusta <tpicoule123@gmail.com>
2024-07-16 16:28:28 +0200

    finished 01 begin 02

--- Cpp_06/ex02/Base.hpp
+++ Cpp_06/ex02/Base.hpp
@@ -1,42 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/27 14:49:02 by tpicoule          #+#    #+#             */
-/*   Updated: 2024/07/01 14:44:17 by tpicoule         ###   ########.fr       */
+/*   Created: 2024/07/01 15:28:43 by tpicoule          #+#    #+#             */
+/*   Updated: 2024/07/01 15:30:52 by tpicoule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERIALIZER
-# define SERIALIZER
-
+#ifndef BASE
+# define BASE
 #include <string>
 #include <iostream>
-#include <stdint.h>
 
-struct Data
-{
-    //struct//
-    std::string name;
-    int i;
-    //struct//    
-};
-
-class Serializer
+class Base
 {
 private:
 public:
-    Serializer();
-    ~Serializer();
-    Serializer &operator=(Serializer const &rhs);
-    Serializer(Serializer const &cpy);
-    static uintptr_t serialize(Data* ptr);
-    static Data* deserialize(uintptr_t raw);
+    Base();
+    virtual ~Base();
+    Base &operator=(Base const &rhs);
+    Base(Base const &cpy);
 };
-
 
 
 
