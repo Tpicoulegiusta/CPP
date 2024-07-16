1cb8b4f2368b75a7d55f8cb97905f779cecb8afb
Tpicoulegiusta <tpicoule123@gmail.com>
2024-07-16 16:28:28 +0200

    scalar finished 4 real

---
+++ Cpp_06/ex01/Serializer.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 14:49:02 by tpicoule          #+#    #+#             */
+/*   Updated: 2024/06/27 14:50:24 by tpicoule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef Serializer
+# define Serializer
+
+#include <string>
+#include <iostream>
+
+class Serializer
+{
+private:
+public:
+    Serializer();
+    ~Serializer();
+};
+
+
+
+
+
+
+
+#endif
