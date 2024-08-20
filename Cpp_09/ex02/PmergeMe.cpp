8e86cc0e3d3cce0f0682a7fe81a73b469918e849
Tpicoulegiusta <tpicoule123@gmail.com>
2024-08-20 17:23:38 +0200

    cpp09 finished inch

--- Cpp_09/ex02/PmergeMe.cpp
+++ Cpp_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 13:37:35 by tpicoule          #+#    #+#             */
-/*   Updated: 2024/08/20 16:15:45 by tpicoule         ###   ########.fr       */
+/*   Updated: 2024/08/20 17:00:18 by tpicoule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,33 +83,10 @@ void ft_min_to_minmax(std::vector<int> &smax, std::vector<int> &smin)
         {
             j = smin[i];
             k = i;
-            //std::cout << i <<std::endl;
         }
     }
-    //std::cout << j << std::endl;
     smax.insert(smax.begin(), j);
-    // std::cout << k << std::endl;
     smin.erase(smin.begin() + k);
-    //std::cout << k << std::endl;
-    //std::cout << j << std:: endl;
-    // for(size_t i = 0; i < smin.size(); i++)
-    // {
-    //     if(smin[i] == j)
-    //         smin.erase(smin.begin() + i);
-    // }
-    // std::cout << "{";
-    // for(size_t i = 0; i < smax.size(); i++)
-    // {
-    //     std::cout << smax[i] << ", "; 
-    // }
-    // std::cout << "}";
-    // std::cout << "\n";
-    // std::cout << "{";
-    // for(size_t i = 0; i < smin.size(); i++)
-    // {
-    //     std::cout << smin[i] << ", ";
-    // }
-    // std::cout << "}";
 }
 
 void ft_min_to_max(std::vector<int> &smax, std::vector<int> &smin)
@@ -121,20 +98,6 @@ void ft_min_to_max(std::vector<int> &smax, std::vector<int> &smin)
         smin.erase(smin.begin() + i);
         i--;//tient compte de la suppression//
     }
-    // std::cout << "\n" << std::endl;
-    // std::cout << "{";
-    // for(size_t i = 0; i < smax.size(); i++)
-    // {
-    //     std::cout << smax[i] << ", "; 
-    // }
-    // std::cout << "}";
-    // std::cout << "\n" << std::endl;
-    // std::cout << "{";
-    // for(size_t i = 0; i < smin.size(); i++)
-    // {
-    //     std::cout << smin[i] << ", "; 
-    // }
-    // std::cout << "}";
 }
 
 
@@ -157,20 +120,11 @@ void sort_value_vec(std::vector<int> &nb)
     {
         nb.push_back(-1);
     }
-    // for(unsigned long i = 0; i < nb.size(); i++)//simple affichage//
-    // {
-    //     std::cout << nb[i] << " ";
-    //     std::cout << " sorting fct" << std::endl;
-    // }
     std::vector<std::pair<int, int> > pairs;//on creer le vecteur stockant les pairs//
     for(size_t i = 0; i < nb.size(); i += 2)//on creer les pairs et on les range dans le container(vector)//
     {
         pairs.push_back(std::make_pair(nb[i], nb[i + 1]));
     }
-    // for(const auto &pair : pairs)//affichage du vecteur//
-    // {
-    //     std::cout << "(" << pair.first << ", " << pair.second << ") " << std::endl;
-    // }
     for(size_t i = 0; i < pairs.size(); i++)//on range dans deux vecteur(smax pour les plus grands et smin pour les plus petits)
     {
         if (pairs[i].first >= pairs[i].second)
@@ -184,18 +138,6 @@ void sort_value_vec(std::vector<int> &nb)
             smax.push_back(pairs[i].second);
         }
     }
-    // std::cout << "after first sort Smax = : {";//affichage smax
-    // for(size_t i = 0; i < smax.size(); i++)
-    // {
-    //     std::cout << smax[i] << ", ";
-    // }
-    // std::cout << "}" << std::endl;
-    // std::cout << "after first sort Smin = : {";//affichage smin
-    // for(size_t i = 0; i < smin.size(); i++)
-    // {
-    //     std::cout << smin[i] << ", ";
-    // }
-    // std::cout << "}" << std::endl;
     ft_recursive(smax, smax.size());//on utilise la recursive pour trier smax//
     ft_min_to_minmax(smax, smin);//on envoie le plus petit de smin dans smax//
     ft_min_to_max(smax, smin);
@@ -205,13 +147,110 @@ void sort_value_vec(std::vector<int> &nb)
         std::cout << smax[i] << " ";
     }
     std::cout << std::endl;
-    // std::cout << "{";
+}
+
+void ft_recursive_deque(std::deque<int> &smax, int n)
+{
+    if(n <= 1)//si 1 element ou si deja trie//
+        return;
+    ft_recursive_deque(smax, n - 1);//on reduit la taille du vecteur pour le ranger petit a petit grace a la recursive//
+    int last = smax[n - 1];
+    int j = n - 2;
+    while(j >= 0 && smax[j] > last)//on compare notre sous-vecteur, on regrade ou va le chiffre a inserer et on decale tout a sa droite si besoin//
+    {
+        smax[j + 1] = smax[j];
+        j--;
+    }
+    smax[j + 1] = last;
+}
+
+void ft_min_to_minmax_deque(std::deque<int> &smax, std::deque<int> &smin)
+{
+    int k = 0;
+    for(size_t i = 0; i < smin.size(); i++)
+    {
+        if(smin[i] < 0)
+            smin.pop_back();
+    }
+    int j = smin[0];
+    for(size_t i = 0; i < smin.size(); i++)
+    {
+        if(smin[i] < j)
+        {
+            j = smin[i];
+            k = i;
+        }
+    }
+    smax.insert(smax.begin(), j);
+    smin.erase(smin.begin() + k);
+}
+
+
+void ft_min_to_max_deque(std::deque<int> &smax, std::deque<int> &smin)
+{
+    for(size_t i = 0; i < smin.size(); i++)
+    {
+        std::deque<int>::iterator it = std::lower_bound(smax.begin(), smax.end(), smin[i]);
+        smax.insert(it, smin[i]);
+        smin.erase(smin.begin() + i);
+        i--;//tient compte de la suppression//
+    }
+}
+
+
+void sort_value_deq(std::deque<int> &deqVersion)
+{
+    for(size_t i = 0; i != deqVersion.size(); i++)
+    {
+        std::cout << deqVersion[i] << std::endl;
+    }
+    std::deque<int> smax;
+    std::deque<int> smin;
+    if(deqVersion.size() % 2 != 0)//on gere le cas d'une suite impair//
+    {
+        deqVersion.push_back(-1);
+    }
+    std::deque<std::pair<int, int> > pairs;//on creer le vecteur stockant les pairs//
+    for(size_t i = 0; i < deqVersion.size(); i += 2)//on creer les pairs et on les range dans le container(vector)//
+    {
+        pairs.push_back(std::make_pair(deqVersion[i], deqVersion[i + 1]));
+    }
+    for(size_t i = 0; i < pairs.size(); i++)//on range dans deux vecteur(smax pour les plus grands et smin pour les plus petits)
+    {
+        if (pairs[i].first >= pairs[i].second)
+        {
+            smax.push_back(pairs[i].first);
+            smin.push_back(pairs[i].second);
+        }
+        if (pairs[i].first < pairs[i].second)
+        {
+            smin.push_back(pairs[i].first);
+            smax.push_back(pairs[i].second);
+        }
+    }
+    // for (auto element : deqVersion)
+    //     std::cout << element << " "; 
+    // std::cout << std::endl;
+    ft_recursive_deque(smax, smax.size());//on utilise la recursive pour trier smax//
+    ft_min_to_minmax_deque(smax, smin);//on envoie le plus petit de smin dans smax//
+    ft_min_to_max_deque(smax, smin);
+    //std::cout << "{";
     // for(size_t i = 0; i < smax.size(); i++)
     // {
-    //     std::cout << smax[i] << ", ";
+    //     std::cout << smax[i];
+    //     std::cout << ", ";
     // }
     // std::cout << "}";
-}
+    // std::cout << std::endl;
+    // std::cout << "{";
+    // for(size_t i = 0; i < smin.size(); i++)
+    // {
+    //     std::cout << smin[i];
+    //     std::cout << ", ";
+    // }
+    // std::cout << "}";
+  }
+
 
 void check_convert_sort_print(int argc, char **argv)
 {
@@ -227,13 +266,19 @@ void check_convert_sort_print(int argc, char **argv)
         std::cout << nb[i] << " ";
     }
     std::cout << std::endl;
+    std::deque<int> deqVersion(nb.begin(), nb.end());
     std::clock_t start_vec = std::clock();
     sort_value_vec(nb);
 	std::clock_t end_vec = std::clock();
+    std::clock_t start_deq = std::clock();
+    sort_value_deq(deqVersion);
+	std::clock_t end_deq = std::clock();
     
     double vecTime = static_cast<double>(end_vec - start_vec) / CLOCKS_PER_SEC * 1000.0;
-    //double deqTime = static_cast<double>(end_deq - start_deq) / CLOCKS_PER_SEC * 1000.0;
+    double deqTime = static_cast<double>(end_deq - start_deq) / CLOCKS_PER_SEC * 1000.0;
 
 	std::cout << "Time to process a range of " << nb.size()
-			<< " elements with a vector: " << vecTime << " us" << std::endl;   
+			<< " elements with a vector: " << vecTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << nb.size()
+			<< " elements with a vector: " << deqTime << " us" << std::endl; 
 }
--- Cpp_09/ex02/PmergeMe.hpp
+++ Cpp_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 13:37:22 by tpicoule          #+#    #+#             */
-/*   Updated: 2024/08/20 16:11:33 by tpicoule         ###   ########.fr       */
+/*   Updated: 2024/08/20 16:56:41 by tpicoule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,13 @@
 std::vector<int> convert_args(int argc, char **argv);
 void check_args(int argc, char **argv);
 void sort_value_vec(std::vector<int> &nb);
+void sort_value_deq(std::deque<int> &deqVersion);
 void check_convert_sort_print(int argc, char **argv);
 void ft_recursive(std::vector<int> &smax, int n);
+void ft_recursive_deque(std::deque<int> &smax, int n);
 void ft_min_to_minmax(std::vector<int> &smax, std::vector<int> &smin);
+void ft_min_to_minmax_deque(std::deque<int> &smax, std::deque<int> &smin);
 void ft_min_to_max(std::vector<int> &smax, std::vector<int> &smin);
+void ft_min_to_max_deque(std::deque<int> &smax, std::deque<int> &smin);
 
 
